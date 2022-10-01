878a9773898a02a1274dd779a894d11f59db4c61
Stepkin Alexander <stepkin@speechpro.com>
2022-10-01 11:39:39 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
+p {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/header-logo/Header-logo.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <h1 class="header__name">
+    Gitogram /
+  </h1>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+.header__name {
+  
+}
+  
+</style>
